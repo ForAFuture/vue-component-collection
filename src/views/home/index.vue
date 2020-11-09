64b7decb1adf796a5c1ef79813aec40882d596b2
liangyongxiang123 <liangyongxiang@yanxishe.com>
2020-11-09 11:34:36 +0800

    添加时间日期格式化方法

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,13 +7,32 @@
         <a href="https://github.com/ForAFuture/vue-component-collection" target="_blank" rel="noopener noreferrer">vue-component-collection</a>
       </p>
     </div>
+    <div class="home-date">
+      <h3>日期格式化</h3>
+      <el-input class="date-input" size="small" v-model="dateValue" placeholder="日期格式：yyyy/MM/dd hh:mm:ss" clearable></el-input>
+      <el-button type="primary" size="small" @click="dateFormat(dateValue)">确认</el-button>
+      <p>格式化结果：{{ formatValue }}</p>
+    </div>
   </div>
 </template>
 <script>
+import utils from '@/utils/index'
 export default {
   name: 'name',
   data () {
-    return {}
+    return {
+      dateValue: '',
+      formatValue: ''
+    }
+  },
+  created () {
+    console.log(utils)
+  },
+  methods: {
+    dateFormat (fmt) {
+      let date = new Date()
+      this.formatValue = utils.dateFormat(date, fmt)
+    }
   }
 }
 </script>
@@ -21,8 +40,13 @@ style <style lang="scss" scoped>
   .home{
     min-height: 100%;
     background: #fff;
-    .home-content{
+    .home-content, .home-date{
       padding: 10px;
+    }
+    .home-date{
+      .date-input{
+        width: 300px;
+      }
     }
   }
 </style>
